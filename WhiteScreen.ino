313f060e0e8461cbb4f166abcace3399532efa59
RacoonDOEggs <66095341+RacoonDOEggs@users.noreply.github.com>
2021-04-20 14:13:01 -0400

    Update WhiteScreen.ino

--- WhiteScreen.ino
+++ WhiteScreen.ino
@@ -1,3 +1,4 @@
+//Déclaration des Pin
 const int enPin = 3;
 const int pwnPin = 6;
 const int potPin = A0;
@@ -7,13 +8,14 @@ int potValue = 0;
 int pwnValue = 0;
 
 void setup(){
+	//Définition des Pin
 	pinMode(enPin, OUTPUT);
 	pinMode(pwnPin, OUTPUT);
 	digitalWrite(enPin, HIGH);
 }
 
 void loop() {
-
+	// Transformation de la valeur du poptetiomètre en signal PWM
 	potValue = analogRead(potPin);
 	pwnValue = map(potValue, 0, 1023, 0, 255);
 	analogWrite(pwnPin, pwnValue);
